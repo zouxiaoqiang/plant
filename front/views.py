da7f940f014d0f503335b4ae3b3051b5e8c0514d
zouxiaoqiang <1182523747@qq.com>
2020-06-23 20:41:06 +0800

    。。

--- front/views.py
+++ front/views.py
@@ -1,21 +1,23 @@
+import builtins
+from builtins import list, set, print, len
+
 from django.views import View
-from .models import Plant,Leaf,Flower
-from django.http import HttpResponse
+from .models import Plant, Leaf, Flower
 from django.shortcuts import render
 
-class LookView(View):
-        def get(self, request):
-                return render(request, 'index.html')
 
-        def post(self, request):
-                leaf_names = request.POST.getlist('leaf_name')
-                results = []
-                for leaf_name in leaf_names:
-                        result = Plant.objects.filter(leafchars__chars=leaf_name)
-                        if results is not []:
-                                results = list(set(result) & (set(results)))
-                        else:
-                                results.append(result)
-                print(results)
-                return HttpResponse('success')
-                # return render(request, 'index2.html', context={'results': results})
+class LookView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+    def post(self, request):
+        leaf_names = request.POST.getlist('leaf_name')
+        results = []
+        for leaf_name in leaf_names:
+            result = Plant.objects.filter(leafchars__chars=leaf_name)
+            if len(results):
+                results = list(set(result) & set(results))
+            else:
+                results = result
+        print(results)
+        return render(request, 'index.html', context={'results': results})
